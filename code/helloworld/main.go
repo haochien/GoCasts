c30aca1d1d8b06ed053faa7550993f1973ee2462
hao <haochien886@gmail.com>
2023-02-11 22:26:48 +0100

    first basic intro in go

--- code/helloworld/main.go
+++ code/helloworld/main.go
@@ -1,7 +1,16 @@
+/*
+package is similar to project or workspace
+each go file should have this line to declair this file belongs to which package
+
+main is used to build an executable package
+if use any other kind of name, then when run build, it won't compile this package (reusable package)
+*/
 package main
 
+// fmt is one of the standard libs in Go
 import "fmt"
 
+//main funtion will always exist if this is executable package
 func main() {
 	fmt.Println("Hi there!")
 }
---
+++ note.md
@@ -0,0 +1,4 @@
+# 1. How to run Go
+
+1. in terminal, cd to current file directory.
+2. type: go run filename.go   (you can type 'go' in terminal to check whether go is properly installed)
